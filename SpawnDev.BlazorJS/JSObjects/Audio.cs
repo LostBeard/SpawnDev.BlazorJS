56655bf003f14d85a1e1d5f9e4d692cf67d81317
Todd <todd@3dgamemarket.net>
2023-03-16 22:23:46 -0400

    Audio JSObject update

--- SpawnDev.BlazorJS/JSObjects/Audio.cs
+++ SpawnDev.BlazorJS/JSObjects/Audio.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.JSInterop;
 
 namespace SpawnDev.BlazorJS.JSObjects {
-    public class Audio : JSObject {
+    public class Audio : HTMLMediaElement
+    {
         public Audio(IJSInProcessObjectReference _ref) : base(_ref) { }
         public Audio(string url) : base(JS.New(nameof(Audio), url)) { }
-        public void Play() => JSRef.CallVoid("play");
+        //public void Play() => JSRef.CallVoid("play");
     }
 }
