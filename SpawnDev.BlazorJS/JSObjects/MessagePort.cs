65413f86e33c271aad131b1a993aa8d21f1a5ea2
Todd <todd@3dgamemarket.net>
2023-06-19 21:14:06 -0400

    Switched  some JSObject event implementations to JSEventCallback

--- SpawnDev.BlazorJS/JSObjects/MessagePort.cs
+++ SpawnDev.BlazorJS/JSObjects/MessagePort.cs
@@ -1,67 +1,25 @@
 ﻿using Microsoft.JSInterop;
 
-namespace SpawnDev.BlazorJS.JSObjects {
-    public interface IMessagePort {
-        event Action OnMessageError;
-        event Action<MessageEvent> OnMessage;
-
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    public interface IMessagePort
+    {
+        JSEventCallback OnMessageError { get; set; }
+        JSEventCallback<MessageEvent> OnMessage { get; set; }
         void Dispose();
         void PostMessage(object message);
         void PostMessage(object message, object[] transfer);
     }
 
     // https://developer.mozilla.org/en-US/docs/Web/API/MessagePort
-
-    public class MessagePort : EventTarget, IMessagePort {
-        CallbackGroup _callbacks = new CallbackGroup();
-
-        public MessagePort(IJSInProcessObjectReference _ref) : base(_ref) {
-            InitEventsHandlers();
-        }
-
-        private void InitEventsHandlers() {
-            _OnMessageCallback = Callback.Create<MessageEvent>((e) => _OnMessage?.Invoke(e), _callbacks);
-            _OnMessageErrorCallback = Callback.Create(() => _OnMessageError?.Invoke(), _callbacks);
-        }
-
-        private Callback _OnMessageCallback;
-        private event Action<MessageEvent> _OnMessage;
-        public event Action<MessageEvent> OnMessage {
-            add {
-                _OnMessage += value;
-                if (_OnMessage.GetInvocationList().Length == 1)
-                    AddEventListener("message", _OnMessageCallback);
-            }
-            remove {
-                if (_OnMessage.GetInvocationList().Length == 1)
-                    RemoveEventListener("message", _OnMessageCallback);
-                _OnMessage -= value;
-            }
-        }
-
-        private Callback _OnMessageErrorCallback;
-        private event Action _OnMessageError;
-        public event Action OnMessageError {
-            add {
-                _OnMessageError += value;
-                if (_OnMessageError.GetInvocationList().Length == 1)
-                    AddEventListener("messageerror", _OnMessageErrorCallback);
-            }
-            remove {
-                if (_OnMessageError.GetInvocationList().Length == 1)
-                    RemoveEventListener("messageerror", _OnMessageErrorCallback);
-                _OnMessageError -= value;
-            }
-        }
-
+    public class MessagePort : EventTarget, IMessagePort
+    {
+        public MessagePort(IJSInProcessObjectReference _ref) : base(_ref) { }
+        public JSEventCallback<MessageEvent> OnMessage { get => new JSEventCallback<MessageEvent>(o => AddEventListener("message", o), o => RemoveEventListener("message", o)); set { } }
+        public JSEventCallback OnMessageError { get => new JSEventCallback(o => AddEventListener("messageerror", o), o => RemoveEventListener("messageerror", o)); set { } }
         public void Start() => JSRef.CallVoid("start");
         public void Close() => JSRef.CallVoid("close");
         public void PostMessage(object message) => JSRef.CallVoid("postMessage", message);
         public void PostMessage(object message, object[] transfer) => JSRef.CallVoid("postMessage", message, transfer);
-
-        protected override void LosingReference()
-        {
-            _callbacks.Dispose();
-        }
     }
 }
