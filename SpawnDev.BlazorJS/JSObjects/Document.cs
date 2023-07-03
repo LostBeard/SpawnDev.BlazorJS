95294f2a59136861f98cca6b9322db39bc33c3e1
Todd <todd@3dgamemarket.net>
2023-07-03 11:14:31 -0400

    Document JSObject update

--- SpawnDev.BlazorJS/JSObjects/Document.cs
+++ SpawnDev.BlazorJS/JSObjects/Document.cs
@@ -3,9 +3,11 @@
 namespace SpawnDev.BlazorJS.JSObjects {
     public class Document : Node {
         public Document(IJSInProcessObjectReference _ref) : base(_ref) { }
-        public Element ActiveElement => JSRef.Get<Element>("sctiveElement");
+        public Element ActiveElement => JSRef.Get<Element>("activeElement");
         public T CreateElement<T>(string tagName) where T : JSObject => JSRef.Call<T>("createElement", tagName);
         public IJSInProcessObjectReference CreateElement(string tagName) => JSRef.Call<IJSInProcessObjectReference>("createElement", tagName);
         public Task ExitFullscreen() => JSRef.CallVoidAsync("exitFullscreen");
+        public Element? FullscreenElement => JSRef.Get<Element?>("fullscreenElement");
+        public JSEventCallback OnFullscreenChange { get => new JSEventCallback(o => AddEventListener("fullscreenchange", o), o => RemoveEventListener("fullscreenchange", o)); set { } }
     }
 }
