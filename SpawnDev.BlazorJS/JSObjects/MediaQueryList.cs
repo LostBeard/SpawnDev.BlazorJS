f46ad7eeafe716bcdedeae518293720d1f77569d
Todd <todd@3dgamemarket.net>
2023-08-18 16:18:32 -0400

    JSObject updates. MediaQueryList, MediaQueryListEvent.

--- SpawnDev.BlazorJS/JSObjects/MediaQueryList.cs
+++ SpawnDev.BlazorJS/JSObjects/MediaQueryList.cs
@@ -2,10 +2,11 @@
 
 namespace SpawnDev.BlazorJS.JSObjects
 {
-    public class MediaQueryList : JSObject
+    public class MediaQueryList : EventTarget
     {
         public MediaQueryList(IJSInProcessObjectReference _ref) : base(_ref) { }
         public bool Matches => JSRef.Get<bool>("matches");
         public string Media => JSRef.Get<string>("media");
+        public JSEventCallback<MediaQueryListEvent> OnChange { get => new JSEventCallback<MediaQueryListEvent>(o => AddEventListener("change", o), o => RemoveEventListener("change", o)); set { } }
     }
 }
---
+++ SpawnDev.BlazorJS/JSObjects/MediaQueryListEvent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.JSInterop;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    public class MediaQueryListEvent : Event
+    {
+        public MediaQueryListEvent(IJSInProcessObjectReference _ref) : base(_ref) { }
+        public bool Matches => JSRef.Get<bool>("matches");
+        public string Media => JSRef.Get<string>("media");
+    }
+}
