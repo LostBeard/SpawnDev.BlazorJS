6d23eef07f7064d7823775ab3c5c188b88ffb3ff
Todd <todd@3dgamemarket.net>
2023-09-08 16:30:39 -0400

    QuickWorker update

--- SpawnDev.BlazorJS/Toolbox/QuickWorker.cs
+++ SpawnDev.BlazorJS/Toolbox/QuickWorker.cs
@@ -4,19 +4,13 @@ namespace SpawnDev.BlazorJS.Toolbox
 {
     public static class QuickWorker
     {
-        static string createObjectURLFuncName = null;
         public static Worker? CreateWorkerFromJS(string js)
         {
-            if (createObjectURLFuncName == null)
-            {
-                if (!BlazorJSRuntime.JS.IsUndefined("URL.createObjectURL")) createObjectURLFuncName = "URL.createObjectURL";
-                else if (!BlazorJSRuntime.JS.IsUndefined("webkitURL.createObjectURL")) createObjectURLFuncName = "webkitURL.createObjectURL";
-                else createObjectURLFuncName = "";
-            }
-            if (string.IsNullOrEmpty(createObjectURLFuncName)) return null;
             using var blob = new Blob(new string[] { js }, new BlobOptions { Type = "application/javascript" });
-            var objUrl = BlazorJSRuntime.JS.Call<string>(createObjectURLFuncName, blob);
-            return new Worker(objUrl);
+            var objUrl = URL.CreateObjectURL(blob);
+            var ret = new Worker(objUrl);
+            URL.RevokeObjectURL(objUrl);
+            return ret;
         }
     }
 }
