b0313ad263d3c35b10a154fe9f58f4159ea215ed
Todd <todd@3dgamemarket.net>
2023-09-18 12:15:19 -0400

    JSObjects update

--- SpawnDev.BlazorJS/JSObjects/HTMLImageElement.cs
+++ SpawnDev.BlazorJS/JSObjects/HTMLImageElement.cs
@@ -1,44 +1,53 @@
 ﻿using Microsoft.JSInterop;
 
-namespace SpawnDev.BlazorJS.JSObjects {
-    public class HTMLImageElement : HTMLElement {
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    public class HTMLImageElement : HTMLElement
+    {
         public int Width => JSRef.Get<int>("width");
         public int Height => JSRef.Get<int>("height");
         public int NaturalWidth => JSRef.Get<int>("naturalWidth");
         public int NaturalHeight => JSRef.Get<int>("naturalHeight");
         public bool Complete => JSRef.Get<bool>("complete");
-
         public string Src { get => JSRef.Get<string>("src"); set => JSRef.Set("src", value); }
-
         public string CrossOrigin { get => JSRef.Get<string>("crossOrigin"); set => JSRef.Set("crossOrigin", value); }
 
         public HTMLImageElement(IJSInProcessObjectReference _ref) : base(_ref) { }
-
         public HTMLImageElement() : base(JS.New("Image")) { }
 
-        public static Task<HTMLImageElement> CreateFromImageAsync(string src) {
+        public static Task<HTMLImageElement> CreateFromImageAsync(string src, string? crossOrigin = null)
+        {
             var t = new TaskCompletionSource<HTMLImageElement>();
-            CreateFromImage(src, t.SetResult);
+            CreateFromImage(src, (image) =>
+            {
+                if (image != null)
+                {
+                    t.SetResult(image);
+                }
+                else
+                {
+                    t.SetException(new Exception("Loading image failed"));
+                }
+            }, crossOrigin);
             return t.Task;
         }
 
-        //public static HTMLImageElement Create()
-        //{
-        //    return CreateNew<HTMLImageElement>("Image");
-        //}
-
-        public static void CreateFromImage(string src, Action<HTMLImageElement> callback) {
+        public static void CreateFromImage(string src, Action<HTMLImageElement?> callback, string? crossOrigin = null)
+        {
             var image = new HTMLImageElement();
             var imageCallbacks = new CallbackGroup();
-            image.AddEventListener("load", Callback.Create(() => {
+            image.AddEventListener("load", Callback.Create(() =>
+            {
                 imageCallbacks.Dispose();
                 callback(image);
             }, imageCallbacks));
-            image.AddEventListener("error", Callback.Create(() => {
+            image.AddEventListener("error", Callback.Create(() =>
+            {
                 imageCallbacks.Dispose();
                 image.Dispose();
                 callback(null);
             }, imageCallbacks));
+            if (crossOrigin != null) image.CrossOrigin = crossOrigin;
             image.Src = src;
         }
 
