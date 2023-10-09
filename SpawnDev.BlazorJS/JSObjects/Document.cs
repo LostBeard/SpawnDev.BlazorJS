6a88505e31b4631ee45577dcf1136d3665058df8
Todd <todd@3dgamemarket.net>
2023-10-08 21:33:56 -0400

    Document JSObject update

--- SpawnDev.BlazorJS/JSObjects/Document.cs
+++ SpawnDev.BlazorJS/JSObjects/Document.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.JSInterop;
 
-namespace SpawnDev.BlazorJS.JSObjects {
-    public class Document : Node {
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    public class Document : Node
+    {
         public Document(IJSInProcessObjectReference _ref) : base(_ref) { }
         public Element ActiveElement => JSRef.Get<Element>("activeElement");
         public T CreateElement<T>(string tagName) where T : JSObject => JSRef.Call<T>("createElement", tagName);
@@ -11,5 +13,7 @@ namespace SpawnDev.BlazorJS.JSObjects {
         public JSEventCallback OnFullscreenChange { get => new JSEventCallback(o => AddEventListener("fullscreenchange", o), o => RemoveEventListener("fullscreenchange", o)); set { } }
         public HTMLHeadElement Head => JSRef.Get<HTMLHeadElement>("head");
         public HTMLBodyElement Body => JSRef.Get<HTMLBodyElement>("body");
+        public NodeList QuerySelectorAll(string selector) => JSRef.Call<NodeList>("querySelectorAll", selector);
+        public Element? QuerySelector(string selector) => JSRef.Call<Element?>("querySelector", selector);
     }
 }
---
+++ SpawnDev.BlazorJS/JSObjects/NodeList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    public class NodeList : JSObject
+    {
+        public Element this[int index] => JSRef.Get<Element>(index);
+        public NodeList(IJSInProcessObjectReference _ref) : base(_ref) { }
+        public int Length => JSRef.Get<int>("length");
+        public void ForEach(Action<Element> fn)
+        {
+            using var cb = Callback.Create(fn);
+            JSRef.CallVoid("forEach", cb);
+        }
+        public Array<Element> Values ()=> JSRef.Call<Array<Element>>("values");
+    }
+}
