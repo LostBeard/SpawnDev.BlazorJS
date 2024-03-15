fb353e80a49ae499fc763122cb379da24089dd2f
Todd Tanner <todd@spawndev.com>
2024-03-15 13:12:55 -0400

    Updated CSSStyleDeclaration

--- SpawnDev.BlazorJS/JSObjects/CSSStyleDeclaration.cs
+++ SpawnDev.BlazorJS/JSObjects/CSSStyleDeclaration.cs
@@ -26,5 +26,36 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// The containing CSSRule.
         /// </summary>
         public CSSRule? ParentRule => JSRef.Get<CSSRule?>("parentRule");
+        /// <summary>
+        /// Removes a property from the CSS declaration block.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public string RemoveProperty(string property) => JSRef.Call<string>("removeProperty", property);
+        /// <summary>
+        /// Modifies an existing CSS property or creates a new CSS property in the declaration block.
+        /// </summary>
+        /// <param name="property">A string representing the CSS property name (hyphen case) to be modified.</param>
+        /// <param name="value">A string allowing the "important" CSS priority to be set. If not specified, treated as the empty string. The following values are accepted: "", "!important"</param>
+        /// <param name="priority">A string containing the new property value. If not specified, treated as the empty string.</param>
+        public void SetProperty(string property, string value = "", string priority = "") => JSRef.CallVoid("setProperty", property, value, priority);
+        /// <summary>
+        /// Returns the property value given a property name.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public string GetPropertyValue(string property) => JSRef.Call<string>("getPropertyValue", property);
+        /// <summary>
+        /// Returns a CSS property name by its index, or the empty string if the index is out-of-bounds.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public string Item(int index) => JSRef.Call<string>("item", index);
+        /// <summary>
+        /// Returns the optional priority, "important".
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public string GetPropertyPriority(string property) => JSRef.Call<string>("getPropertyPriority", property);
     }
 }
