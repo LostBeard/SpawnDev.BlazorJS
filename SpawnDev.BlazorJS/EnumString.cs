36fc6f371dfe9bffec735840b715081eb599cbaa
Todd Tanner <todd@spawndev.com>
2024-04-02 12:36:26 -0400

    EnumString update

--- SpawnDev.BlazorJS/EnumString.cs
+++ SpawnDev.BlazorJS/EnumString.cs
@@ -1,5 +1,4 @@
-﻿using SpawnDev.BlazorJS.JSObjects;
-using SpawnDev.BlazorJS.JsonConverters;
+﻿using SpawnDev.BlazorJS.JsonConverters;
 using System.Text.Json.Serialization;
 
 namespace SpawnDev.BlazorJS
@@ -12,7 +11,7 @@ namespace SpawnDev.BlazorJS
         public abstract string? Text { get; set; }
     }
     /// <summary>
-    /// Enum string class is a class that allows easy conversion to and from Enums, strings, and ints<br />
+    /// Enum string class is a class that allows easy conversion to and from Enums, and strings<br />
     /// Serializes to: if (null) null; else Text, which can be null, an enum string, or any string value<br />
     /// Deserializes to: if (null) null; else EnumString<br />
     /// Setting Text to the deserialized string value<br />
@@ -26,6 +25,10 @@ namespace SpawnDev.BlazorJS
     [JsonConverter(typeof(EnumStringConverter))]
     public class EnumString<T> : EnumString where T : struct, Enum
     {
+        private T? _Value { get; set; }
+        /// <summary>
+        /// Enum value
+        /// </summary>
         public T? Value
         {
             get => _Value;
@@ -36,35 +39,22 @@ namespace SpawnDev.BlazorJS
                 {
                     _Value = value;
                     _Text = value!.Value.ToString();
-                    _Int = (int)Convert.ChangeType(value!.Value, value!.Value.GetTypeCode());
                 }
                 else
                 {
-                    _Text = null;
                     _Value = null;
-                    _Int = null;
+                    _Text = null;
                 }
             }
         }
-        public int? Int
-        {
-            get => _Int;
-            set
-            {
-                if (value == null)
-                {
-                    Value = null;
-                }
-                else
-                {
-                    Value = (T)Enum.ToObject(typeof(T), value.Value);
-                }
-            }
-        }
+        /// <summary>
+        /// Returns true if the Enum value IsDefined
+        /// </summary>
         public bool IsDefined { get; private set; }
         private string? _Text = null;
-        private T? _Value { get; set; }
-        private int? _Int { get; set; }
+        /// <summary>
+        /// string value
+        /// </summary>
         public override string? Text
         {
             get => _Text;
@@ -75,26 +65,17 @@ namespace SpawnDev.BlazorJS
                 {
                     IsDefined = true;
                     _Value = (T)e!;
-                    _Int = (int)Convert.ChangeType(_Value!.Value, _Value!.Value.GetTypeCode());
                 }
                 else
                 {
                     IsDefined = false;
                     _Value = null;
-                    _Int = null;
                 }
             }
         }
         public EnumString() { }
-        public EnumString(int value) { Int = value; }
         public EnumString(T value) { Value = value; }
         public EnumString(string value) { Text = value; }
-        // int
-        public static implicit operator EnumString<T>(int value) => new EnumString<T>(value);
-        public static implicit operator int(EnumString<T> value) => value.Int!.Value;   // will throw if !Valid
-        // int?
-        public static implicit operator EnumString<T>?(int? value) => value == null ? null : new EnumString<T>(value.Value);
-        public static implicit operator int?(EnumString<T>? value) => value == null || !value.IsDefined ? null : value.Int;
         // T
         public static implicit operator EnumString<T>(T value) => new EnumString<T>(value);
         public static implicit operator T(EnumString<T> value) => value.Value!.Value;
