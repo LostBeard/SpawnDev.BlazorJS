dd712e9d53029353d2beae359642a854ca8e4f82
Todd Tanner <todd@spawndev.com>
2024-04-06 08:45:25 -0400

    Added missing IDBTransaction events

--- SpawnDev.BlazorJS/JSObjects/IDBTransaction.cs
+++ SpawnDev.BlazorJS/JSObjects/IDBTransaction.cs
@@ -36,5 +36,17 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// For an active transaction, commits the transaction. Note that this doesn't normally have to be called — a transaction will automatically commit when all outstanding requests have been satisfied and no new requests have been made. commit() can be used to start the commit process without waiting for events from outstanding requests to be dispatched.
         /// </summary>
         public void Commit() => JSRef.CallVoid("commit");
+        /// <summary>
+        /// An event fired when the IndexedDB transaction is aborted. Also available via the onabort property; this event bubbles to IDBDatabase.
+        /// </summary>
+        public JSEventCallback<Event> OnAbort { get => new JSEventCallback<Event>("abort", AddEventListener, RemoveEventListener); set { } }
+        /// <summary>
+        /// An event fired when the transaction successfully completes. Also available via the oncomplete property.
+        /// </summary>
+        public JSEventCallback<Event> OnComplete { get => new JSEventCallback<Event>("complete", AddEventListener, RemoveEventListener); set { } }
+        /// <summary>
+        /// An event fired when a request returns an error and the event bubbles up to the connection object (IDBDatabase). Also available via the onerror property.
+        /// </summary>
+        public JSEventCallback<Event> OnError { get => new JSEventCallback<Event>("error", AddEventListener, RemoveEventListener); set { } }
     }
 }
