21ed5d805662fc67fd8335c36a804677edc9e5ef
Todd Tanner <todd@spawndev.com>
2024-04-11 17:05:20 -0400

    RTCIceServer changed Urls from string to Union

--- SpawnDev.BlazorJS/JSObjects/WebRTC/RTCIceServer.cs
+++ SpawnDev.BlazorJS/JSObjects/WebRTC/RTCIceServer.cs
@@ -1,18 +1,30 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace SpawnDev.BlazorJS.JSObjects.WebRTC {
-    // https://developer.mozilla.org/en-US/docs/Web/API/RTCIceServer
-    public class RTCIceServer {
+namespace SpawnDev.BlazorJS.JSObjects.WebRTC
+{
+    /// <summary>
+    /// Describes one server which may be used by the ICE agent; these are typically STUN and/or TURN servers. If this isn't specified, the connection attempt will be made with no STUN or TURN server available, which limits the connection to local peers.<br />
+    /// https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#iceservers
+    /// </summary>
+    public class RTCIceServer
+    {
+        /// <summary>
+        /// The credential to use when logging into the server. This is only used if the object represents a TURN server.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string Credential { get; set; } = null;
-
+        [JsonPropertyName("credential")]
+        public string? Credential { get; set; }
+        /// <summary>
+        /// This required property is either a single string or an array of strings, each specifying a URL which can be used to connect to the server.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string CredentialType { get; set; } = null;
-
+        [JsonPropertyName("urls")]
+        public Union<string, string[]>? Urls { get; set; }
+        /// <summary>
+        /// If the object represents a TURN server, then this is the username to use during the authentication.
+        /// </summary>
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string Urls { get; set; } = null;
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string Username { get; set; } = null;
+        [JsonPropertyName("username")]
+        public string? Username { get; set; }
     }
 }
