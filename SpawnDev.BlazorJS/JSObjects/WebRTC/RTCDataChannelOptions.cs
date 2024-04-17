ffb0c2bac63e30ab9ca5ef7b5788ec290d25c08c
Todd Tanner <todd@spawndev.com>
2024-04-17 10:18:31 -0400

    RTCMediaEncoding update

--- SpawnDev.BlazorJS/JSObjects/WebRTC/RTCDataChannelOptions.cs
+++ SpawnDev.BlazorJS/JSObjects/WebRTC/RTCDataChannelOptions.cs
@@ -1,23 +1,25 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace SpawnDev.BlazorJS.JSObjects.WebRTC {
-    public class RTCDataChannelOptions {
+namespace SpawnDev.BlazorJS.JSObjects.WebRTC
+{
+    public class RTCDataChannelOptions
+    {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public bool? Ordered { get; set; } = null;
+        public bool? Ordered { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public ushort? MaxPacketLifeTime { get; set; } = null;
+        public ushort? MaxPacketLifeTime { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public ushort? MaxRetransmits { get; set; } = null;
+        public ushort? MaxRetransmits { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string Protocol { get; set; } = null;
+        public string? Protocol { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public bool? Negotiated { get; set; } = null;
+        public bool? Negotiated { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public ushort? Id { get; set; } = null;
+        public ushort? Id { get; set; }
     }
 }
