3ee852061db051185395079607c3fd637d4157eb
Todd Tanner <todd@spawndev.com>
2024-06-13 16:08:10 -0400

    Added additional constructors to File JSObject

--- SpawnDev.BlazorJS/JSObjects/File.cs
+++ SpawnDev.BlazorJS/JSObjects/File.cs
@@ -27,6 +27,86 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// <param name="options">An options object containing optional attributes for the file</param>
         public File(IEnumerable<Union<ArrayBuffer, TypedArray, DataView, Blob, string>> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
         /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        public File(IEnumerable<ArrayBuffer> bits, string name) : base(JS.New(nameof(File), bits, name)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        /// <param name="options">An options object containing optional attributes for the file</param>
+        public File(IEnumerable<ArrayBuffer> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        public File(IEnumerable<TypedArray> bits, string name) : base(JS.New(nameof(File), bits, name)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        /// <param name="options">An options object containing optional attributes for the file</param>
+        public File(IEnumerable<TypedArray> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        public File(IEnumerable<DataView> bits, string name) : base(JS.New(nameof(File), bits, name)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        /// <param name="options">An options object containing optional attributes for the file</param>
+        public File(IEnumerable<DataView> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        public File(IEnumerable<Blob> bits, string name) : base(JS.New(nameof(File), bits, name)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        /// <param name="options">An options object containing optional attributes for the file</param>
+        public File(IEnumerable<Blob> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        public File(IEnumerable<string> bits, string name) : base(JS.New(nameof(File), bits, name)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        /// <param name="options">An options object containing optional attributes for the file</param>
+        public File(IEnumerable<string> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        public File(IEnumerable<byte[]> bits, string name) : base(JS.New(nameof(File), bits, name)) { }
+        /// <summary>
+        /// The File() constructor creates a new File object instance.
+        /// </summary>
+        /// <param name="bits">An iterable object such as an Array, having ArrayBuffers, TypedArrays, DataViews, Blobs, strings, or a mix of any of such elements, that will be put inside the File. Note that strings here are encoded as UTF-8, unlike the usual JavaScript UTF-16 strings.</param>
+        /// <param name="name">A string representing the file name or the path to the file.</param>
+        /// <param name="options">An options object containing optional attributes for the file</param>
+        public File(IEnumerable<byte[]> bits, string name, FileOptions options) : base(JS.New(nameof(File), bits, name, options)) { }
+
+
+        /// <summary>
         /// Returns the name of the file referenced by the File object.
         /// </summary>
         public string Name => JSRef.Get<string>("name");
