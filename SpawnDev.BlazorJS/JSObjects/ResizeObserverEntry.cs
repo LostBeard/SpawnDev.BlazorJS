d4f3f3df5695251e7fa9103b1ebcea7c333333b4
Todd Tanner <todd@spawndev.com>
2024-07-14 12:09:19 -0400

    Updated ResizeObserverEntry JSObject

--- SpawnDev.BlazorJS/JSObjects/ResizeObserverEntry.cs
+++ SpawnDev.BlazorJS/JSObjects/ResizeObserverEntry.cs
@@ -1,16 +1,37 @@
 ﻿using Microsoft.JSInterop;
 
-namespace SpawnDev.BlazorJS.JSObjects {
-    public class ResizeObserverSize {
-        public double InlineSize { get; set; }
-        public double BlockSize { get; set; }
-    }
-
-    public class ResizeObserverEntry : JSObject {
-        public List<ResizeObserverSize> BorderBoxSize => JSRef!.Get<List<ResizeObserverSize>>("borderBoxSize");
-        public List<ResizeObserverSize> ContentBoxSize => JSRef!.Get<List<ResizeObserverSize>>("contentBoxSize");
-        public List<ResizeObserverSize> DevicePixelContentBoxSize => JSRef!.Get<List<ResizeObserverSize>>("devicePixelContentBoxSize");
-        public DOMRectReadOnly ContentRect => JSRef!.Get<DOMRectReadOnly>("contentRect");
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// The ResizeObserverEntry interface represents the object passed to the ResizeObserver() constructor's callback function, which allows you to access the new dimensions of the Element or SVGElement being observed.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserverEntry
+    /// </summary>
+    public class ResizeObserverEntry : JSObject
+    {
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="_ref"></param>
         public ResizeObserverEntry(IJSInProcessObjectReference _ref) : base(_ref) { }
+        /// <summary>
+        /// An array of objects containing the new border box size of the observed element when the callback is run.
+        /// </summary>
+        public List<ResizeObserverSize> BorderBoxSize => JSRef!.Get<List<ResizeObserverSize>>("borderBoxSize");
+        /// <summary>
+        /// An array of objects containing the new content box size of the observed element when the callback is run.
+        /// </summary>
+        public List<ResizeObserverSize> ContentBoxSize => JSRef!.Get<List<ResizeObserverSize>>("contentBoxSize");
+        /// <summary>
+        /// An array of objects containing the new content box size in device pixels of the observed element when the callback is run.
+        /// </summary>
+        public List<ResizeObserverSize> DevicePixelContentBoxSize => JSRef!.Get<List<ResizeObserverSize>>("devicePixelContentBoxSize");
+        /// <summary>
+        /// A DOMRectReadOnly object containing the new size of the observed element when the callback is run. Note that this is now a legacy property that is retained in the spec for backward-compatibility reasons only.
+        /// </summary>
+        public DOMRectReadOnly ContentRect => JSRef!.Get<DOMRectReadOnly>("contentRect");
+        /// <summary>
+        /// A reference to the Element or SVGElement being observed.
+        /// </summary>
+        public Element Target => JSRef!.Get<Element>("target");
     }
 }
---
+++ SpawnDev.BlazorJS/JSObjects/ResizeObserverSize.cs
@@ -0,0 +1,18 @@
+﻿namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// The content box size of the observed element<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserverEntry/contentBoxSize#value
+    /// </summary>
+    public class ResizeObserverSize
+    {
+        /// <summary>
+        /// The length of the observed element's content box in the block dimension. For boxes with a horizontal writing-mode, this is the vertical dimension, or height; if the writing-mode is vertical, this is the horizontal dimension, or width.
+        /// </summary>
+        public double BlockSize { get; set; }
+        /// <summary>
+        /// The length of the observed element's content box in the inline dimension. For boxes with a horizontal writing-mode, this is the horizontal dimension, or width; if the writing-mode is vertical, this is the vertical dimension, or height.
+        /// </summary>
+        public double InlineSize { get; set; }
+    }
+}
