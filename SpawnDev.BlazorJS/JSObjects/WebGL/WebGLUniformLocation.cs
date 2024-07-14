4e0c9dcb05a70bf19e717473823d0a8e9e947e3f
Todd Tanner <todd@spawndev.com>
2024-07-14 12:20:58 -0400

    Updated WebGLUniformLocation JSObject

--- SpawnDev.BlazorJS/JSObjects/WebGL/WebGLUniformLocation.cs
+++ SpawnDev.BlazorJS/JSObjects/WebGL/WebGLUniformLocation.cs
@@ -2,9 +2,16 @@
 
 namespace SpawnDev.BlazorJS.JSObjects
 {
-
+    /// <summary>
+    /// The WebGLUniformLocation interface is part of the WebGL API and represents the location of a uniform variable in a shader program.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/WebGLUniformLocation
+    /// </summary>
     public class WebGLUniformLocation : JSObject
     {
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="_ref"></param>
         public WebGLUniformLocation(IJSInProcessObjectReference _ref) : base(_ref) { }
     }
 }
