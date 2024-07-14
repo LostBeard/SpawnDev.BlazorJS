285e98d1a1b7ac01d5c79b74660290132aedd27f
Todd Tanner <todd@spawndev.com>
2024-07-14 12:05:47 -0400

    Updated ResizeObserver JSObject

---
+++ SpawnDev.BlazorJS/JSObjects/ObserveOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// An options object allowing you to set options for the observation.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver/observe#options
+    /// </summary>
+    public class ObserveOptions
+    {
+        /// <summary>
+        /// Sets which box model the observer will observe changes to. Possible values are:<br/>
+        /// "content-box" - Size of the content area as defined in CSS. (default)<br/>
+        /// "border-box" - Size of the box border area as defined in CSS.<br/>
+        /// "device-pixel-content-box" - The size of the content area as defined in CSS, in device pixels, before applying any CSS transforms on the element or its ancestors.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Box {  get; set; }
+    }
+}
--- SpawnDev.BlazorJS/JSObjects/ResizeObserver.cs
+++ SpawnDev.BlazorJS/JSObjects/ResizeObserver.cs
@@ -1,14 +1,64 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace SpawnDev.BlazorJS.JSObjects {
-    public class ResizeObserver : JSObject {
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// The ResizeObserver interface reports changes to the dimensions of an Element's content or border box, or the bounding box of an SVGElement.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+    /// </summary>
+    public class ResizeObserver : JSObject
+    {
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="_ref"></param>
         public ResizeObserver(IJSInProcessObjectReference _ref) : base(_ref) { }
+        /// <summary>
+        /// Creates and returns a new ResizeObserver object.
+        /// </summary>
+        /// <param name="callback"></param>
         public ResizeObserver(ActionCallback<ResizeObserverEntry[]> callback) : base(JS.New(nameof(ResizeObserver), callback)) { }
-        public ResizeObserver(Callback callback) : base(JS.New(nameof(ResizeObserver), callback)) { }
-        public void Observe(IJSInProcessObjectReference el) => JSRef!.CallVoid("observe", el);
-        public void Unobserve(IJSInProcessObjectReference el) => JSRef!.CallVoid("unobserve", el);
-        public void Observe(ElementReference el) => JSRef!.CallVoid("observe", el);
+        /// <summary>
+        /// Creates and returns a new ResizeObserver object.
+        /// </summary>
+        /// <param name="callback"></param>
+        public ResizeObserver(ActionCallback callback) : base(JS.New(nameof(ResizeObserver), callback)) { }
+        /// <summary>
+        /// Unobserves all observed Element targets of a particular observer.
+        /// </summary>
+        public void Disconnect() => JSRef!.CallVoid("disconnect");
+        /// <summary>
+        /// Initiates the observing of a specified Element.
+        /// </summary>
+        /// <param name="target"></param>
+        public void Observe(ElementReference target) => JSRef!.CallVoid("observe", target);
+        /// <summary>
+        /// Initiates the observing of a specified Element.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="options"></param>
+        public void Observe(Element target, ObserveOptions options) => JSRef!.CallVoid("observe", target, options);
+        /// <summary>
+        /// Initiates the observing of a specified Element.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="options"></param>
+        public void Observe(ElementReference target, ObserveOptions options) => JSRef!.CallVoid("observe", target, options);
+        /// <summary>
+        /// Initiates the observing of a specified Element.
+        /// </summary>
+        /// <param name="target"></param>
+        public void Observe(Element target) => JSRef!.CallVoid("observe", target);
+        /// <summary>
+        /// Ends the observing of a specified Element.
+        /// </summary>
+        /// <param name="el"></param>
         public void Unobserve(ElementReference el) => JSRef!.CallVoid("unobserve", el);
+        /// <summary>
+        /// Ends the observing of a specified Element.
+        /// </summary>
+        /// <param name="el"></param>
+        public void Unobserve(Element el) => JSRef!.CallVoid("unobserve", el);
     }
 }
