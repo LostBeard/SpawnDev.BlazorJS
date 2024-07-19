2e2c4030620a70b37554fd9add10e9c583c0bd07
Todd Tanner <todd@spawndev.com>
2024-07-19 11:36:20 -0400

    Updated JSEventCallback to support up to 7 parameters

--- SpawnDev.BlazorJS/JEventCallbackInfo.cs
+++ SpawnDev.BlazorJS/JEventCallbackInfo.cs
@@ -89,4 +89,82 @@
         /// <param name="callback"></param>
         public JEventCallbackInfo(Action<T1, T2, T3, T4> action, ActionCallback<T1, T2, T3, T4> callback) => (Action, Callback) = (action, callback);
     }
+    /// <summary>
+    /// Container for a typed Callback Action pair
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    internal class JEventCallbackInfo<T1, T2, T3, T4, T5> : JSEventCallbackInfoBase
+    {
+        /// <summary>
+        /// The Callback
+        /// </summary>
+        public ActionCallback<T1, T2, T3, T4, T5> Callback { get; }
+        /// <summary>
+        /// The Action
+        /// </summary>
+        public Action<T1, T2, T3, T4, T5> Action { get; }
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callback"></param>
+        public JEventCallbackInfo(Action<T1, T2, T3, T4, T5> action, ActionCallback<T1, T2, T3, T4, T5> callback) => (Action, Callback) = (action, callback);
+    }
+    /// <summary>
+    /// Container for a typed Callback Action pair
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    internal class JEventCallbackInfo<T1, T2, T3, T4, T5, T6> : JSEventCallbackInfoBase
+    {
+        /// <summary>
+        /// The Callback
+        /// </summary>
+        public ActionCallback<T1, T2, T3, T4, T5, T6> Callback { get; }
+        /// <summary>
+        /// The Action
+        /// </summary>
+        public Action<T1, T2, T3, T4, T5, T6> Action { get; }
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callback"></param>
+        public JEventCallbackInfo(Action<T1, T2, T3, T4, T5, T6> action, ActionCallback<T1, T2, T3, T4, T5, T6> callback) => (Action, Callback) = (action, callback);
+    }
+    /// <summary>
+    /// Container for a typed Callback Action pair
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    /// <typeparam name="T4"></typeparam>
+    /// <typeparam name="T5"></typeparam>
+    /// <typeparam name="T6"></typeparam>
+    /// <typeparam name="T7"></typeparam>
+    internal class JEventCallbackInfo<T1, T2, T3, T4, T5, T6, T7> : JSEventCallbackInfoBase
+    {
+        /// <summary>
+        /// The Callback
+        /// </summary>
+        public ActionCallback<T1, T2, T3, T4, T5, T6, T7> Callback { get; }
+        /// <summary>
+        /// The Action
+        /// </summary>
+        public Action<T1, T2, T3, T4, T5, T6, T7> Action { get; }
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callback"></param>
+        public JEventCallbackInfo(Action<T1, T2, T3, T4, T5, T6, T7> action, ActionCallback<T1, T2, T3, T4, T5, T6, T7> callback) => (Action, Callback) = (action, callback);
+    }
 }
