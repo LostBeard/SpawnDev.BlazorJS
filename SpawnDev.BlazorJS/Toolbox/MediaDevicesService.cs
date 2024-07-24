3b8fc0d708dcb6d78114b11544939a5888d87a58
Todd Tanner <todd@spawndev.com>
2024-07-24 12:53:52 -0400

    Fixed MediaDevicesService empty labels in Firefox. Code cleanup

--- SpawnDev.BlazorJS/Toolbox/MediaDevicesService.cs
+++ SpawnDev.BlazorJS/Toolbox/MediaDevicesService.cs
@@ -1,19 +1,71 @@
 ﻿using SpawnDev.BlazorJS.JSObjects;
 
-namespace SpawnDev.BlazorJS.Toolbox
+namespace SpawnDev.BlazorJS.SpeerNet
 {
+    /// <summary>
+    /// Provides access to navigator.mediaDevices
+    /// </summary>
     public class MediaDevicesService : IAsyncBackgroundService, IDisposable
     {
+        /// <summary>
+        /// Returns when the service is ready
+        /// </summary>
+        public Task Ready => _Ready ??= InitAsync();
+        private Task? _Ready = null;
+        /// <summary>
+        /// navigator.mediaDevices
+        /// </summary>
         public MediaDevices MediaDevices { get; private set; }
+        /// <summary>
+        /// Current list of MediaDeviceInfo
+        /// </summary>
         public List<MediaDeviceInfo> DeviceInfos { get; private set; } = new List<MediaDeviceInfo>();
+        /// <summary>
+        /// Returns true if navigator.mediaDevices.enumerateDevices is supported
+        /// </summary>
         public bool MediaShareSupported { get; private set; }
+        /// <summary>
+        /// Returns true if navigator.mediaDevices.getDisplayMedia is supported
+        /// </summary>
         public bool DesktopShareSupported { get; private set; }
+        /// <summary>
+        /// Returns true if MediaDevices is supported
+        /// </summary>
         public bool Supported { get; private set; }
+        /// <summary>
+        /// Returns true if a device without an empty label has not been seen
+        /// </summary>
         public bool AreDevicesHidden { get; private set; } = true;
-
+        /// <summary>
+        /// Returns a list of supported known audio video types using MediaRecorder.IsTypeSupported
+        /// </summary>
+        public List<string> MediaRecorderAudioVideoFormats => _MediaRecorderAudioVideoFormats.Value;
+        /// <summary>
+        /// Known video types that will be tested
+        /// </summary>
+        public static List<string> VideoTypes { get; } = new List<string> { "webm", "mp4", "x-matroska", "ogg" };
+        /// <summary>
+        /// Known video codecs that will be tested
+        /// </summary>
+        public static List<string> VideoCodecs { get; } = new List<string> { "vp9", "vp9.0", "vp8", "vp8.0", "avc1", "av1", "h265", "h.265", "h264", "h.264", "mpeg", "theora" };
+        /// <summary>
+        /// Known audio types that will be tested
+        /// </summary>
+        public static List<string> AudioTypes { get; } = new List<string> { "webm", "mp3", "mp4", "x-matroska", "ogg", "wav" };
+        /// <summary>
+        /// Known audio codecs that will be tested
+        /// </summary>
+        public static List<string> AudioCodecs { get; } = new List<string> { "opus", "vorbis", "aac", "mpeg", "mp4a", "pcm" };
+        /// <summary>
+        /// Returns true if a device has been seen without an empty label
+        /// </summary>
+        public bool DevicesSeenUnhidden { get; private set; } = false;
+        public delegate void MediaDeviceInfoEvent(MediaDeviceInfo mediaDeviceInfo);
+        public event MediaDeviceInfoEvent OnMediaDeviceFound;
+        public event MediaDeviceInfoEvent OnMediaDeviceLost;
         public delegate void DeviceInfosChangedDelegate();
         public event DeviceInfosChangedDelegate OnDeviceInfosChanged;
-        BlazorJSRuntime JS;
+        private BlazorJSRuntime JS;
         public MediaDevicesService(BlazorJSRuntime js)
         {
             JS = js;
@@ -27,20 +79,18 @@ namespace SpawnDev.BlazorJS.Toolbox
             DebugCodecCheck();
 #endif
         }
-
-        public static List<string> VideoTypes = new List<string> { "webm", "mp4", "x-matroska", "ogg" };
-        public static List<string> VideoCodecs = new List<string> { "vp9", "vp9.0", "vp8", "vp8.0", "avc1", "av1", "h265", "h.265", "h264", "h.264", "mpeg", "theora" };
-        public static List<string> AudioTypes = new List<string> { "webm", "mp3", "mp4", "x-matroska", "ogg", "wav" };
-        public static List<string> AudioCodecs = new List<string> { "opus", "vorbis", "aac", "mpeg", "mp4a", "pcm" };
-
-        void DebugCodecCheck()
+        private async Task InitAsync()
+        {
+            if (!Supported) return;
+            await UpdateDeviceList();
+        }
+        private void DebugCodecCheck()
         {
             MediaRecorderAudioVideoFormats.ForEach(Console.WriteLine);
             MediaRecorderAudioFormats.ForEach(Console.WriteLine);
             MediaRecorderVideoFormats.ForEach(Console.WriteLine);
         }
-
-        public Lazy<List<string>> _MediaRecorderVideoFormats = new Lazy<List<string>>(() =>
+        private Lazy<List<string>> _MediaRecorderVideoFormats = new Lazy<List<string>>(() =>
         {
             var supportVideoFormats = new List<string>();
             foreach (var videoType in VideoTypes)
@@ -57,9 +107,11 @@ namespace SpawnDev.BlazorJS.Toolbox
             }
             return supportVideoFormats;
         });
+        /// <summary>
+        /// Returns a list of supported known video types using MediaRecorder.IsTypeSupported
+        /// </summary>
         public List<string> MediaRecorderVideoFormats => _MediaRecorderVideoFormats.Value;
-
-        public Lazy<List<string>> _MediaRecorderAudioFormats = new Lazy<List<string>>(() =>
+        private Lazy<List<string>> _MediaRecorderAudioFormats = new Lazy<List<string>>(() =>
         {
             var supportAudioFormats = new List<string>();
             foreach (var audioType in AudioTypes)
@@ -76,8 +128,10 @@ namespace SpawnDev.BlazorJS.Toolbox
             }
             return supportAudioFormats;
         });
+        /// <summary>
+        /// Returns a list of supported known audio types using MediaRecorder.IsTypeSupported
+        /// </summary>
         public List<string> MediaRecorderAudioFormats => _MediaRecorderAudioFormats.Value;
-
         public Lazy<List<string>> _MediaRecorderAudioVideoFormats = new Lazy<List<string>>(() =>
         {
             var supportAudioVideoFormats = new List<string>();
@@ -98,22 +152,17 @@ namespace SpawnDev.BlazorJS.Toolbox
             }
             return supportAudioVideoFormats;
         });
-        public List<string> MediaRecorderAudioVideoFormats => _MediaRecorderAudioVideoFormats.Value;
-        public Task Ready => _Ready ??= InitAsync();
-        private Task? _Ready = null;
-        async Task InitAsync()
-        {
-            if (!Supported) return;
-            await UpdateDeviceList();
-        }
-
-        void MediaDevices_OnDeviceChange()
+        private void MediaDevices_OnDeviceChange()
         {
             _ = UpdateDeviceList();
         }
-
+        /// <summary>
+        /// Returns true if this instance has been disposed
+        /// </summary>
         public bool IsDisposed { get; private set; } = false;
-
+        /// <summary>
+        /// Disposes resources
+        /// </summary>
         public void Dispose()
         {
             if (IsDisposed) return;
@@ -124,63 +173,72 @@ namespace SpawnDev.BlazorJS.Toolbox
                 MediaDevices.Dispose();
             }
         }
-
-        public async Task<MediaStream?> GetMediaStream(string? videoDeviceId = null, string? audioDeviceId = null)
-        {
-            MediaStream? stream = null;
-            try
-            {
-                stream = await MediaDevices.GetMediaDeviceStream(videoDeviceId, audioDeviceId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("GetMediaDeviceStream error: " + ex.Message);
-            }
-            return stream;
-        }
-
-        // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        /// <summary>
+        /// Updates the device list, optionally allowing a prompt if necessary
+        /// </summary>
+        /// <param name="allowAsk"></param>
+        /// <param name="video"></param>
+        /// <param name="audio"></param>
+        /// <returns></returns>
         public async Task<bool> UpdateDeviceList(bool allowAsk = false, bool video = true, bool audio = true)
         {
             if (!MediaShareSupported) return false;
-            AreDevicesHidden = await MediaDevices.AreDevicesHidden();
-            await _UpdateDeviceList();
-            if (!AreDevicesHidden || !allowAsk) return !AreDevicesHidden;
+            var areDevicesHidden = await MediaDevices.AreDevicesHidden();
             try
             {
-                using var stream = await MediaDevices.GetUserMedia(video, audio);
-                if (stream != null)
+                if (!areDevicesHidden)
                 {
-                    stream.RemoveAllTracks();
-                    stream.Dispose();
+                    await _UpdateDeviceList();
+                    return !areDevicesHidden;
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"EXCEPTION getUserMedia: {ex.Message}");
-            }
-            await _UpdateDeviceList();
-            AreDevicesHidden = await MediaDevices.AreDevicesHidden();
-            return !AreDevicesHidden;
-        }
-
-        async Task _UpdateDeviceList()
-        {
-            var t = DeviceInfos.Select(o => o.DeviceId).ToList();
-            var countNow = DeviceInfos.Count;
-            DeviceInfos = (await MediaDevices.EnumerateDevices()).Where(o => !string.IsNullOrEmpty(o.DeviceId)).ToList();
-            var changed = countNow != DeviceInfos.Count;
-            if (!changed)
-            {
-                foreach (var d in DeviceInfos)
+                if (!DevicesSeenUnhidden && !allowAsk) return !areDevicesHidden;
+                // getting device infos is going to require getting a MediaStream first
+                try
                 {
-                    if (!t.Contains(d.DeviceId))
+                    // Firefox has been seen reverting to basic mediadeviceinfo when no streams are active, even if one was allowed
+                    // update infos now
+                    using var stream = await MediaDevices.GetUserMedia(video, audio);
+                    if (stream != null)
                     {
-                        changed = true;
-                        break;
+                        await _UpdateDeviceList();
+                        DevicesSeenUnhidden = true;
+                        areDevicesHidden = false;
+                        stream.RemoveAllTracks();
+                        stream.Dispose();
                     }
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"EXCEPTION getUserMedia: {ex.Message}");
+                }
             }
+            finally
+            {
+                AreDevicesHidden = areDevicesHidden;
+            }
+            return !areDevicesHidden;
+        }
+        private async Task _UpdateDeviceList()
+        {
+            var oldDeviceInfos = DeviceInfos;
+            var oldDeviceIds = DeviceInfos.Select(o => o.DeviceId).ToList();
+            var newDeviceInfos = (await MediaDevices.EnumerateDevices()).Where(o => !string.IsNullOrEmpty(o.DeviceId) && !string.IsNullOrEmpty(o.Label)).ToList();
+            var newDeviceIds = newDeviceInfos.Select(o => o.DeviceId).ToList();
+            var lostDeviceIds = oldDeviceIds.Except(newDeviceIds);
+            var foundDeviceIds = newDeviceIds.Except(oldDeviceIds);
+            var lostDevices = DeviceInfos.Where(o => lostDeviceIds.Contains(o.DeviceId)).ToList();
+            var foundDevices = newDeviceInfos.Where(o => foundDeviceIds.Contains(o.DeviceId)).ToList();
+            DeviceInfos = newDeviceInfos;
+            foreach (var item in lostDevices)
+            {
+                OnMediaDeviceLost?.Invoke(item);
+            }
+            foreach (var item in foundDevices)
+            {
+                OnMediaDeviceFound?.Invoke(item);
+            }
+            oldDeviceInfos.ToArray().DisposeAll();
+            var changed = lostDevices.Count > 0 || foundDevices.Count > 0;
             if (changed)
             {
                 OnDeviceInfosChanged?.Invoke();
