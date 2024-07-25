14bfdc2364a1a2206c19677b64cdb469f659cc88
Todd Tanner <todd@spawndev.com>
2024-07-24 21:09:16 -0400

    Demo update

--- SpawnDev.BlazorJS.Demo/Program.cs
+++ SpawnDev.BlazorJS.Demo/Program.cs
@@ -7,7 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazorJSRuntime();
+builder.Services.AddBlazorJSRuntime(out var JS);
+
+JS.Log($"InstanceId: {JS.InstanceId}");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
