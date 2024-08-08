14e4271d9879a2f06924335497f4f603798a07ab
Todd Tanner <todd@spawndev.com>
2024-08-08 00:27:58 -0400

    Updated UnitTests

--- SpawnDev.BlazorJS.Demo/Program.cs
+++ SpawnDev.BlazorJS.Demo/Program.cs
@@ -1,31 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpawnDev.BlazorJS;
-using SpawnDev.BlazorJS.JSObjects;
 using SpawnDev.BlazorJS.Demo;
-using SpawnDev.BlazorJS.Toolbox;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddBlazorJSRuntime(out var JS);
-
-JS.Log($"InstanceId: {JS.InstanceId}");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 var host = await builder.Build().StartBackgroundServices();
-
-#if DEBUG
-
+#if DEBUG && false
+// null-conditional test
 JS.Set("__test", new { name = "something", config = new { visible = true } });
-
-var isUndefined1 = JS.Get<bool>("__test.config.visible");
-var isUndefined2 = JS.Get<bool>("__test.config?.visible");
-var isUndefined3 = JS.Get<bool>("__test.config2?.visible");
-var isUndefined4 = JS.Get<bool>("__test1?.config2.visible1");
-var nmt = true;
+var testVar1 = JS.Get<bool>("__test.config.visible");
+var testVar2 = JS.Get<bool>("__test.config?.visible");
+var testVar3 = JS.Get<bool>("__test.config2?.visible");
+var testVar4 = JS.Call<bool>("__test.config2?.doesNotExist");
+JS.Set("__test.config2?.doesNotExist", true);
+JS.Delete("__test");
 #endif
-
 await host.BlazorJSRunAsync();
