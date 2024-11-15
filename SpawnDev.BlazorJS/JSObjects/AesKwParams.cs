bc787cd48a61404ff0d9cde877663fbccfd72344
Todd Tanner <todd@spawndev.com>
2024-11-15 10:27:53 -0500

    Added AES-KW params object

---
+++ SpawnDev.BlazorJS/JSObjects/AesKwParams.cs
@@ -0,0 +1,13 @@
+﻿namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// AES-KW parameters
+    /// </summary>
+    public class AesKwParams : EncryptParams
+    {
+        /// <summary>
+        /// A string. This should be set to AES-KW.
+        /// </summary>
+        public override string Name { get; set; } = "AES-KW";
+    }
+}
