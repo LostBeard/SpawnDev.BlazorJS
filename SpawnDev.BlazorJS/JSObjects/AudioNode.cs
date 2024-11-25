ea21620b35209d9c96cefa1af5ffb009c4195f07
Todd Tanner <todd@spawndev.com>
2024-11-25 09:41:33 -0500

    Added instance Connect and Disconnect methods to AudioNode

--- SpawnDev.BlazorJS/JSObjects/AudioNode.cs
+++ SpawnDev.BlazorJS/JSObjects/AudioNode.cs
@@ -19,5 +19,25 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// </summary>
         /// <returns></returns>
         public AudioNode Connect(AudioNode audioNode) => JSRef!.Call<AudioNode>("connect", audioNode);
+        /// <summary>
+        /// The connect() method of the AudioNode interface lets you connect one of the node's outputs to a target, which may be either another AudioNode (thereby directing the sound data to the specified node) or an AudioParam, so that the node's output data is automatically used to change the value of that parameter over time.
+        /// </summary>
+        /// <param name="audioParam"></param>
+        public void Connect(AudioParam audioParam) => JSRef!.CallVoid("connect", audioParam);
+        /// <summary>        
+        /// Allows us to disconnect the current node from another one it is already connected to.
+        /// </summary>
+        public void Disconnect() => JSRef!.CallVoid("disconnect");
+        /// <summary>
+        /// Allows us to disconnect the current node from another one it is already connected to.
+        /// </summary>
+        /// <param name="audioNode"></param>
+        /// <returns></returns>
+        public void Disconnect(AudioNode audioNode) => JSRef!.CallVoid("disconnect", audioNode);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="audioParam"></param>
+        public void Disconnect(AudioParam audioParam) => JSRef!.CallVoid("disconnect", audioParam);
     }
 }
