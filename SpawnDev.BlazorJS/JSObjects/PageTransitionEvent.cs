c35662c258201669e50408ba014ea8e21f5b0896
Todd Tanner <todd@spawndev.com>
2025-04-02 12:05:03 -0400

    Added summary xml

--- SpawnDev.BlazorJS/JSObjects/PageTransitionEvent.cs
+++ SpawnDev.BlazorJS/JSObjects/PageTransitionEvent.cs
@@ -2,9 +2,17 @@
 
 namespace SpawnDev.BlazorJS.JSObjects
 {
+    /// <summary>
+    /// The PageTransitionEvent event object is available inside handler functions for the pageshow and pagehide events, fired when a document is being loaded or unloaded.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/PageTransitionEvent
+    /// </summary>
     public class PageTransitionEvent : Event
     {
+        ///<inheritdoc/>
         public PageTransitionEvent(IJSInProcessObjectReference _ref) : base(_ref) { }
+        /// <summary>
+        /// Indicates if the document is loading from a cache.
+        /// </summary>
         public bool Persisted => JSRef!.Get<bool>("persisted");
     }
 }
