0547c4d30c41f048b4fa50bab86907e1de8a73af
Todd Tanner <todd@spawndev.com>
2025-04-19 10:21:23 -0400

    Added missing SyncManager memebrs. Issue #43

--- SpawnDev.BlazorJS/JSObjects/SyncManager.cs
+++ SpawnDev.BlazorJS/JSObjects/SyncManager.cs
@@ -13,6 +13,16 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// </summary>
         /// <param name="_ref"></param>
         public SyncManager(IJSInProcessObjectReference _ref) : base(_ref) { }
-
+        /// <summary>
+        /// The SyncManager interface of the Background Synchronization API provides an interface for registering and listing sync registrations.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public Task Register(string tag) =>  JSRef!.CallVoidAsync("register", tag);
+        /// <summary>
+        /// The getTags() method of the SyncManager interface returns a list of developer-defined identifiers for SyncManager registrations.
+        /// </summary>
+        /// <returns></returns>
+        public Task<string[]> GetTags() => JSRef!.CallAsync<string[]>("getTags");
     }
 }
