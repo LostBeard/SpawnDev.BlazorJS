f8612bd4f07ae9c3a84b6ab99419dbea6d403eec
Todd Tanner <todd@spawndev.com>
2025-07-05 22:34:41 -0400

    Added SharedWorkerOptions and SharedWorker missing constructor

--- SpawnDev.BlazorJS/JSObjects/SharedWorker.cs
+++ SpawnDev.BlazorJS/JSObjects/SharedWorker.cs
@@ -20,14 +20,20 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// <summary>
         /// The SharedWorker() constructor creates a SharedWorker object that executes the script at the specified URL. This script must obey the same-origin policy.
         /// </summary>
-        /// <param name="url"></param>
+        /// <param name="url">A string representing the URL of the script the worker will execute. It must obey the same-origin policy.</param>
         public SharedWorker(string url) : base(JS.New(nameof(SharedWorker), url)) { }
         /// <summary>
         /// The SharedWorker() constructor creates a SharedWorker object that executes the script at the specified URL. This script must obey the same-origin policy.
         /// </summary>
-        /// <param name="url"></param>
-        /// <param name="name"></param>
+        /// <param name="url">A string representing the URL of the script the worker will execute. It must obey the same-origin policy.</param>
+        /// <param name="name">A string specifying an identifying name for the SharedWorkerGlobalScope representing the scope of the worker, which is useful for creating new instances of the same SharedWorker and debugging.</param>
         public SharedWorker(string url, string name) : base(JS.New(nameof(SharedWorker), url, name)) { }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">A string representing the URL of the script the worker will execute. It must obey the same-origin policy.</param>
+        /// <param name="options"></param>
+        public SharedWorker(string url, SharedWorkerOptions options) : base(JS.New(nameof(SharedWorker), url, options)) { }
         /// <summary>
         /// Fires when an error occurs in the shared worker.
         /// </summary>
---
+++ SpawnDev.BlazorJS/JSObjects/SharedWorkerOptions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// An object containing option properties that can set when creating the SharedWorker instance.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/SharedWorker/SharedWorker#options
+    /// </summary>
+    public class SharedWorkerOptions
+    {
+        /// <summary>
+        /// A string specifying the type of worker to create. The value can be classic or module. If not specified, the default used is classic.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Type { get; set; }
+        /// <summary>
+        /// A string specifying the type of credentials to use for the worker. The value can be omit, same-origin, or include. If not specified, or if type is classic, the default used is same-origin (only include credentials for same-origin requests).
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Credentials { get; set; }
+        /// <summary>
+        /// A string specifying an identifying name for the SharedWorkerGlobalScope representing the scope of the worker, which is useful for creating new instances of the same SharedWorker and debugging.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Name { get; set; }
+        /// <summary>
+        /// A string indicating which SameSite cookies should be available to the worker. Can have one of the following two values:<br/>
+        /// "all" - SameSite=Strict, SameSite=Lax, and SameSite=None cookies will all be available to the worker. This option is only supported in first-party contexts, and is the default in first-party contexts.<br/>
+        /// "none" - Only SameSite=None cookies will be available to the worker. This option is supported in first-party and third-party contexts, and is the default in third-party contexts.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? SameSiteCookies { get; set; }
+    }
+}
