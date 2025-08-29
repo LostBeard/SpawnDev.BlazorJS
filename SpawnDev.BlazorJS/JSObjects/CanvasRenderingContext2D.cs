627d63081c0195ce56c2a9104d52b7bc1bf17375
Todd Tanner <todd@spawndev.com>
2025-08-28 23:04:18 -0400

    Used SharedByteClampedArray in PutImageBytes

--- SpawnDev.BlazorJS/JSObjects/CanvasRenderingContext2D.cs
+++ SpawnDev.BlazorJS/JSObjects/CanvasRenderingContext2D.cs
@@ -271,9 +271,9 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// <param name="dy"></param>
         public void PutImageBytes(byte[] srcBytes, int srcWidth, int srcHeight, int dx = 0, int dy = 0)
         {
-            // using a SharedByteArray prevents a copy of the data from being made before the draw. 
-            using var sharedUint8Array = (SharedByteArray)srcBytes;
-            using var imageData = ImageData.FromUint8Array(sharedUint8Array, srcWidth, srcHeight);
+            // using a SharedByteClampedArray prevents a copy of the data from being made before the draw. 
+            using var sharedUint8Array = (SharedByteClampedArray)srcBytes;
+            using var imageData = new ImageData(sharedUint8Array, srcWidth, srcHeight);
             PutImageData(imageData, dx, dy);
         }
         /// <summary>
@@ -281,9 +281,9 @@ namespace SpawnDev.BlazorJS.JSObjects
         /// </summary>
         public void PutImageBytes(byte[] imageBytes, int srcWidth, int srcHeight, int dx, int dy, int dirtyX, int dirtyY, int dirtyWidth, int dirtyHeight)
         {
-            // using a SharedByteArray prevents a copy of the data from being made before the draw. 
-            using var sharedUint8Array = (SharedByteArray)imageBytes;
-            using var imageData = ImageData.FromUint8Array(sharedUint8Array, srcWidth, srcHeight);
+            // using a new SharedByteClampedArray prevents a copy of the data from being made before the draw. 
+            using var sharedUint8Array = (SharedByteClampedArray)imageBytes;
+            using var imageData = new ImageData(sharedUint8Array, srcWidth, srcHeight);
             PutImageData(imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight);
         }
         /// <summary>
