96b0197a8348ca035cf6ac78493f50b4c9fb9bc2
Todd Tanner <todd@spawndev.com>
2025-09-17 11:09:12 -0400

    Added XRWebGLBinding base

---
+++ SpawnDev.BlazorJS/JSObjects/WebXR/XRWebGLBinding.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// The XRWebGLBinding interface is used to create layers that have a GPU backend.<br/>
+    /// https://developer.mozilla.org/en-US/docs/Web/API/XRWebGLBinding
+    /// </summary>
+    public class XRWebGLBinding : JSObject
+    {
+        /// <inheritdoc/>
+        public XRWebGLBinding(IJSInProcessObjectReference _ref) : base(_ref) { }
+
+        // TODO
+    }
+}
