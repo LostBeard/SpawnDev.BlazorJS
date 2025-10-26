7dd03f315d30ccfbfb01b04503e253d1cd93dc4b
Björn Gåvefeldt <swizzy@xeupd.com>
2025-10-26 09:49:22 +0100

    Updated HID types to match the documentation with all types currently defined

---
+++ SpawnDev.BlazorJS/JSObjects/HIDDeviceFilter.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// HID Device filter options
+    /// https://developer.mozilla.org/en-US/docs/Web/API/HID/requestDevice#parameters
+    /// </summary>
+    public class HIDDeviceFilter
+    {
+        /// <summary>
+        /// An integer representing the vendorId of the requested HID device
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? VendorId { get; set; }
+        /// <summary>
+        /// An integer representing the productId of the requested HID device.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? ProductId { get; set; }
+        /// <summary>
+        /// An integer representing the usage page component of the HID usage of the requested device. The usage for a top level collection is used to identify the device type.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? UsagePage { get; set; }
+        /// <summary>
+        /// An integer representing the usage ID component of the HID usage of the requested device.
+        /// </summary>
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? Usage { get; set; }
+    }
+}
---
+++ SpawnDev.BlazorJS/JSObjects/HIDReportInfo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// A HIDReportInfo represents one input, output, or feature report within the report descriptor.
+    /// https://wicg.github.io/webhid/#dom-hidreportinfo
+    /// </summary>
+    public class HIDReportInfo : JSObject
+    {
+        public HIDReportInfo(IJSInProcessObjectReference _ref) : base(_ref)
+        {
+        }
+
+        /// <summary>
+        /// A HID interface uses report IDs if it prepends a one-byte identification prefix to each report transfer. The reportId member is the prefix for this report if the interface uses report IDs, or 0 otherwise.
+        /// </summary>
+        public byte ReportId => JSRef!.Get<byte>("reportId");
+
+        /// <summary>
+        /// A sequence of HIDReportItem representing the fields of this report.
+        /// </summary>
+        public IEnumerable<HIDReportItem> Items => JSRef!.Get<IEnumerable<HIDReportItem>>("items");
+    }
+}
