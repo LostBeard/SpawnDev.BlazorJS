d6410b2fa4d88291f6731096770776d63942114c
Todd Tanner <todd@spawndev.com>
2025-11-12 18:40:06 -0500

    Version  2.42.0

--- SpawnDev.BlazorJS.Demo/Program.cs
+++ SpawnDev.BlazorJS.Demo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SpawnDev.BackgroundServices;
 using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.Demo;
 using SpawnDev.BlazorJS.JSObjects;
@@ -11,13 +12,20 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazorJSRuntime(out var JS);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-var host = await builder.Build().StartBackgroundServices();
+var host = builder.Build();
+
+await host.Services.StartBackgroundServices();
 
 JS.Set("_testit", (string msg) =>
 {
     Console.WriteLine($"_testit called with message: {msg}");
 });
-#if DEBUG && true
+
+
+var nmt = true;
+var http = host.Services.GetRequiredService<HttpClient>();
+var gg = true;
+#if DEBUG && false
 long usedManagedMemory = GC.GetTotalMemory(forceFullCollection: true);
 long heapSize = HeapView.GetHeapBufferSize();
 JS.Log($"usedManagedMemory: {usedManagedMemory}/{heapSize}");
