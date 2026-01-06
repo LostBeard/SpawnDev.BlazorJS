850ef3e5ddf01cf4426a86d9785e034a3e327491
Todd Tanner <todd@spawndev.com>
2026-01-05 22:19:45 -0500

    Marked MessagePort as TransferableRequired

--- SpawnDev.BlazorJS/JSObjects/MessagePort.cs
+++ SpawnDev.BlazorJS/JSObjects/MessagePort.cs
@@ -39,7 +39,7 @@ namespace SpawnDev.BlazorJS.JSObjects
     /// The MessagePort interface of the Channel Messaging API represents one of the two ports of a MessageChannel, allowing messages to be sent from one port and listening out for them arriving at the other.<br/>
     /// https://developer.mozilla.org/en-US/docs/Web/API/MessagePort
     /// </summary>
-    [Transferable]
+    [Transferable(TransferRequired = true)]
     public class MessagePort : EventTarget, IMessagePort
     {
         /// <summary>
