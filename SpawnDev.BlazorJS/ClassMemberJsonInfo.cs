0c3f2d0fe546f1bf3dfb2b8ac63c9b2d560521b8
Todd Tanner <todd@spawndev.com>
2026-01-12 11:20:04 -0500

    Added missing XML documentation

--- SpawnDev.BlazorJS/ClassMemberJsonInfo.cs
+++ SpawnDev.BlazorJS/ClassMemberJsonInfo.cs
@@ -5,29 +5,60 @@ using System.Text.Json.Serialization;
 namespace SpawnDev.BlazorJS
 {
     /// <summary>
-    /// Determines information used for Json serialization for a specified type
+    /// Determines information used for Json serialization for a specified Propert or Field
     /// </summary>
     public class ClassMemberJsonInfo
     {
+        /// <summary>
+        /// Type default value
+        /// </summary>
         public object? DefaultValue { get; private set; }
+        /// <summary>
+        /// Field or Property name
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// JsonIgnoeAttribute
+        /// </summary>
         public JsonIgnoreAttribute? JsonIgnoreAttribute { get; set; }
+        /// <summary>
+        /// JsonPropertyNameAttribute
+        /// </summary>
         public JsonPropertyNameAttribute? JsonPropertyNameAttribute { get; set; }
-        public bool? JsonIncludeAttribute { get; set; }
+        /// <summary>
+        /// ProeprtyInfo is a property
+        /// </summary>
         public PropertyInfo? PropertyInfo { get; private set; }
+        /// <summary>
+        /// FieldInfo if a field
+        /// </summary>
         public FieldInfo? FieldInfo { get; private set; }
+        /// <summary>
+        /// New instance usign FieldInfo
+        /// </summary>
+        /// <param name="fieldInfo"></param>
         public ClassMemberJsonInfo(FieldInfo fieldInfo)
         {
             FieldInfo = fieldInfo;
             Name = FieldInfo.Name;
             DefaultValue = FieldInfo.FieldType.GetDefaultValue();
         }
+        /// <summary>
+        /// New instance usign PropertyInfo
+        /// </summary>
+        /// <param name="propertyInfo"></param>
         public ClassMemberJsonInfo(PropertyInfo propertyInfo)
         {
             PropertyInfo = propertyInfo;
             Name = PropertyInfo.Name;
             DefaultValue = PropertyInfo.PropertyType.GetDefaultValue();
         }
+        /// <summary>
+        /// Returns if the Property/Field should be written when serialized
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="jsonSerializerOptions"></param>
+        /// <returns></returns>
         public bool GetShouldWrite(object? value, JsonSerializerOptions? jsonSerializerOptions = null)
         {
             if (JsonIgnoreAttribute == null) return true;
@@ -36,6 +67,11 @@ namespace SpawnDev.BlazorJS
             if (JsonIgnoreAttribute.Condition == JsonIgnoreCondition.WhenWritingDefault && value == DefaultValue) return false;
             return true;
         }
+        /// <summary>
+        /// Get's the Json version of the Property/Field name
+        /// </summary>
+        /// <param name="jsonSerializerOptions"></param>
+        /// <returns></returns>
         public string GetJsonName(JsonSerializerOptions? jsonSerializerOptions = null)
         {
             if (JsonPropertyNameAttribute != null) return JsonPropertyNameAttribute.Name;
