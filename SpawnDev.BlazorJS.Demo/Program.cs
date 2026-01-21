4efd7dd530276c4d7551d02e6792624f92b907dd
Todd Tanner <todd@spawndev.com>
2026-01-21 10:43:33 -0500

    Commented Program.cs

--- SpawnDev.BlazorJS.Demo/Program.cs
+++ SpawnDev.BlazorJS.Demo/Program.cs
@@ -6,11 +6,12 @@ using SpawnDev.BlazorJS.Demo;
 using SpawnDev.BlazorJS.Demo.UnitTests;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddBlazorJSRuntime(out var JS);
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// add BlazorJSRuntime (Javascript interop)
+builder.Services.AddBlazorJSRuntime(out var JS);
+
+// unit test services and the services they use
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<BlazorJSUnitTest>();
 builder.Services.AddSingleton<CryptoService>();
 builder.Services.AddSingleton<FileSystemAPIService>();
@@ -18,4 +19,8 @@ builder.Services.AddSingleton<TypedArrayTests>();
 builder.Services.AddSingleton<MediaTests>();
 builder.Services.AddSingleton<CacheTests>();
 
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// start the app using BlazorJSRunAsync
 await builder.Build().BlazorJSRunAsync();
