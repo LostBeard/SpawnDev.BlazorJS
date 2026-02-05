68bebecdb4485b87a0a452b255d1c7dea40d284c
Todd Tanner <todd@spawndev.com>
2026-02-05 10:53:09 -0500

    Added GPUPowerPreference

---
+++ SpawnDev.BlazorJS/JSObjects/WebGPU/GPUPowerPreference.cs
@@ -0,0 +1,23 @@
+﻿using SpawnDev.BlazorJS.JsonConverters;
+using System.Text.Json.Serialization;
+
+namespace SpawnDev.BlazorJS.JSObjects
+{
+    /// <summary>
+    /// Power preference options for GPUAdapter request.
+    /// </summary>
+    [JsonConverter(typeof(EnumStringConverterFactory))]
+    public enum GPUPowerPreference
+    {
+        /// <summary>
+        /// Low power usage is preferred.
+        /// </summary>
+        [JsonPropertyName("low-power")]
+        LowPower,
+        /// <summary>
+        /// High performance is preferred.
+        /// </summary>
+        [JsonPropertyName("high-performance")]
+        HighPerformance,
+    }
+}
