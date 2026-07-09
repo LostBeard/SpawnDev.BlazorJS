3235dab19db52f564f10cf7e648dc94b56f86c75
Todd Tanner <todd@spawndev.com>
2026-07-09 13:00:11 -0400

    CrossOriginWindowTest cleanup and documenting

--- SpawnDev.BlazorJS.Demo/UnitTests/CrossOriginWindowTests.cs
+++ SpawnDev.BlazorJS.Demo/UnitTests/CrossOriginWindowTests.cs
@@ -6,7 +6,11 @@ namespace SpawnDev.BlazorJS.Demo.UnitTests
 {
     public class CrossOriginWindowTests(BlazorJSRuntime JS)
     {
-
+        /// <summary>
+        /// Test for LostBeard/SpawnDev.BlazorJS#62 - Window Closed throws a SecurityError
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="UnsupportedTestException"></exception>
         [TestMethod]
         public async Task CrossOriginWindowTest()
         {
@@ -18,10 +22,12 @@ namespace SpawnDev.BlazorJS.Demo.UnitTests
 
             if (popup is not null)
             {
+                // small delay used in this test to make sure the window is flagged as
+                // cross-origin before the test runs, which is part of the cause of the original issue #62
                 await Task.Delay(2000);
 
                 // Close the popup in 5 seconds
-                _ = Async.RunAsync(async () =>
+                Async.Run(async () =>
                 {
                     await Task.Delay(5000);
                     try
@@ -31,7 +37,10 @@ namespace SpawnDev.BlazorJS.Demo.UnitTests
                     catch { }
                 });
 
+                // try set focused
                 popup!.Focus();
+
+                // wait for the window to close
                 while (!popup.Closed)
                     await Task.Delay(1000);
             }
